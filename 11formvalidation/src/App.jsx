941b144aee2910b4e00a4df95eb804a6e70ecc85
Beigh-Farooq <beighfarooq199@gmail.com>
2025-03-22 14:40:55 +0530

    Form Validation in React without yup and with yup

---
+++ 11formvalidation/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import FormWithoutYup from './components/FormwithoutYup'
+
+import './App.css'
+import FormWithYup from './components/FormWithYup'
+
+function App() {
+  
+
+  return (
+    <>
+      {/* <FormWithoutYup/> */}
+      <FormWithYup/>
+    </>
+  )
+}
+
+export default App
